99d1378e0b7f66cf7e9d7152d468e16f7cc76c15
Anderson Gabriel Ferreira <anderson.gabriel.ferreira@gmail.com>
2022-02-25 18:13:36 -0300

    Initial commit

---
+++ app/src/main/java/com/example/codelab_compose_layout/MainActivity.kt
@@ -0,0 +1,63 @@
+package com.example.codelab_compose_layout
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.*
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.CompositionLocalProvider
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.codelab_compose_layout.ui.theme.CodelabcomposelayoutTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            CodelabcomposelayoutTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colors.background
+                ) {
+                    Greeting("Android")
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun PhotographCard(){
+    Column {
+        Text("Alfred Sisley", fontWeight = FontWeight.Bold)
+        //LocalContentAlpha is defining opacity level of its children
+        CompositionLocalProvider(LocalContentAlpha provides ContentAlpha.medium) {
+            Text("3 minutes ago", style = MaterialTheme.typography.body2)
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String) {
+    Text(text = "Hello $name!")
+}
+
+@Preview(showBackground = true)
+@Composable
+fun DefaultPreview() {
+    CodelabcomposelayoutTheme {
+        Greeting("Android")
+    }
+}
+
+@Preview
+@Composable
+fun PhotographerCardPreview(){
+    CodelabcomposelayoutTheme {
+        PhotographCard()
+    }
+}
