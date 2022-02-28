4284174cab9925a788b521f9a5c391b58fc81113
Anderson Gabriel Ferreira <anderson.gabriel.ferreira@gmail.com>
2022-02-28 18:57:59 -0300

    WIP

--- app/src/main/java/com/example/codelab_compose_layout/MainActivity.kt
+++ app/src/main/java/com/example/codelab_compose_layout/MainActivity.kt
@@ -3,14 +3,20 @@ package com.example.codelab_compose_layout
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.*
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.CompositionLocalProvider
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.codelab_compose_layout.ui.theme.CodelabcomposelayoutTheme
 
 class MainActivity : ComponentActivity() {
@@ -23,7 +29,7 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colors.background
                 ) {
-                    Greeting("Android")
+                    LayoutsCodelab()
                 }
             }
         }
@@ -31,32 +37,55 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun PhotographCard(){
-    Column {
-        Text("Alfred Sisley", fontWeight = FontWeight.Bold)
-        //LocalContentAlpha is defining opacity level of its children
-        CompositionLocalProvider(LocalContentAlpha provides ContentAlpha.medium) {
-            Text("3 minutes ago", style = MaterialTheme.typography.body2)
+fun LayoutsCodelab(){
+    Scaffold{ innerPadding ->
+        Text(text = "Hi there", modifier = Modifier.padding(innerPadding))
+    }
+}
+
+@Composable
+fun PhotographCard(modifier: Modifier = Modifier) {
+    Row(
+        modifier = Modifier
+            .padding(8.dp)
+            .clip(RoundedCornerShape(4.dp))
+            .background(MaterialTheme.colors.surface)
+            .clickable(onClick = { /* Ignoring click */ })
+            .padding(16.dp)) {
+        Surface(
+            modifier = Modifier.size(50.dp),
+            shape = CircleShape,
+            color = MaterialTheme.colors.onSurface.copy(alpha = 0.2f)
+        ) {
+            //image goes here
+
+        }
+        Column(
+            modifier = Modifier
+                .padding(start = 8.dp)
+                .align(Alignment.CenterVertically)
+        ) {
+            Text("Alfred Sisley", fontWeight = FontWeight.Bold)
+            //LocalContentAlpha is defining opacity level of its children
+            CompositionLocalProvider(LocalContentAlpha provides ContentAlpha.medium) {
+                Text("3 minutes ago", style = MaterialTheme.typography.body2)
+            }
         }
     }
 }
 
-@Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
-}
 
 @Preview(showBackground = true)
 @Composable
-fun DefaultPreview() {
+fun LayoutsCodelabPreview() {
     CodelabcomposelayoutTheme {
-        Greeting("Android")
+        LayoutsCodelab()
     }
 }
 
 @Preview(showBackground = true)
 @Composable
-fun PhotographerCardPreview(){
+fun PhotographerCardPreview() {
     CodelabcomposelayoutTheme {
         PhotographCard()
     }
